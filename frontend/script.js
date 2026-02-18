93d01377e834b4a01f5251fb4ab5f9ea85727ceb
jaswanth <jaswanth@jaswanths-MacBook-Air.local>
2026-02-18 20:16:57 +0530

    Added PWA support

--- frontend/script.js
+++ frontend/script.js
@@ -67,3 +67,11 @@ userInput.addEventListener("keypress", function (e) {
         sendMessage();
     }
 });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch(err => console.log("Service Worker Error:", err));
+  });
+}
